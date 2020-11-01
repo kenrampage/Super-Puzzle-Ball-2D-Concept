9e54d628ea9a483e682df510b6ec082f7be315fd
Ken Rampage <73263295+kenrampage@users.noreply.github.com>
2020-11-01 13:55:09 -0800

    Converted to New Input System + added cooldown effect

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SPB;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+namespace SPB
+{
+    public class MenuManager : MonoBehaviour
+
+
+    {
+        public GameObject pauseMenuUI;
+        public GameInput controls;
+
+        private void Awake()
+        {
+            controls = new GameInput();
+            controls.Player.Menu.performed += context => GameStateChanger();
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void OnEnable()
+        {
+            controls.Enable();
+        }
+
+        private void OnDisable()
+        {
+            controls.Disable();
+        }
+
+        public void ResumeGame()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameManager.gameIsPaused = false;
+            GameObject.Find("Main Camera").GetComponent<Animator>().SetBool("isGamePaused", false);
+
+        }
+
+        public void PauseGame()
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameManager.gameIsPaused = true;
+            GameObject.Find("Main Camera").GetComponent<Animator>().SetBool("isGamePaused", true);
+        }
+
+
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void QuitToMenu()
+        {
+            Debug.Log("Player has quit to the menu");
+            Time.timeScale = 1f;
+            GameManager.gameIsPaused = false;
+            SceneManager.LoadScene(0);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Player has quit the game");
+            Application.Quit();
+
+
+        }
+
+        public void GameStateChanger()
+        {
+
+            if (GameManager.gameIsPaused)
+            {
+                ResumeGame();
+
+            }
+            else
+            {
+                PauseGame();
+            }
+
+        }
+
+        private void TestOutput(string content)
+        {
+            Debug.Log("The Output is :" + content);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using SPB;
+using UnityEngine.InputSystem;
 
 namespace SPB
 {
@@ -12,47 +13,123 @@ namespace SPB
         private float squarePivotRotation;
         private float mouseRotationZ;
         private Vector2 mouseDifference;
+        private Vector2 movementInput;
         private float xInput;
+        private float yInput;
 
         public GameObject wandPivotObject;
         public GameObject environmentPivotObject;
+        public GameObject gameManager;
+        public GameInput controls;
+        public SpriteRenderer spriteRenderer;
+
+
         public float impulseForce = 5;
         public float rotationSpeed;
-        
+        public float boostCooldown = 5;
+        private float boostNextFireTime = 0;
+        private float boostLastFireTime = 0;
 
+        public float boostCooldownLeftPercent;
 
-        // Start is called before the first frame update
+        private void Awake()
+        {
+            controls = new GameInput();
+            controls.Player.Move.performed += context => SetVectorInput(context.ReadValue<Vector2>());
+            controls.Player.Move.canceled += context => SetVectorInput(new Vector2(0, 0));
+            controls.Player.Boost.performed += context => BoostPlayer();
+        }
+
+        private void OnEnable()
+        {
+            controls.Enable();
+        }
+
+        private void OnDisable()
+        {
+            controls.Disable();
+        }
+
         void Start()
         {
             playerRb = GetComponent<Rigidbody2D>();
-
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
-        // Update is called once per frame
         void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                playerRb.AddForce(mouseDifference * impulseForce, ForceMode2D.Impulse);
-            }
+
+
+
         }
 
         private void FixedUpdate()
         {
-            mouseDifference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            mouseDifference.Normalize();
-            mouseRotationZ = Mathf.Atan2(mouseDifference.y, mouseDifference.x) * Mathf.Rad2Deg;
-            xInput = Input.GetAxis("Horizontal");
-            environmentPivotObject.transform.Rotate(Vector3.back, xInput * rotationSpeed);
 
-            RotateOnPivot(mouseDifference, wandPivotObject);
+            RotatePlayer(wandPivotObject);
+            RotateEnvironment(xInput);
+            SetMouseDirection();
+
+            if (boostNextFireTime > Time.time)
+            {
+                boostCooldownLeftPercent = (boostNextFireTime - Time.time) / boostCooldown;
+                spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1 - boostCooldownLeftPercent);
+                GameObject.Find("Wand").GetComponent<SpriteRenderer>().enabled = false;
+
+
+            }
+            else
+            {
+                boostCooldownLeftPercent = 1;
+                spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1);
+                GameObject.Find("Wand").GetComponent<SpriteRenderer>().enabled = true;
+            }
 
         }
 
-        public void RotateOnPivot(Vector2 rotateVector, GameObject pivotObjectName)
+        public void RotatePlayer(GameObject pivotObjectName)
         {
-            float rotationZ = Mathf.Atan2(rotateVector.y, rotateVector.x) * Mathf.Rad2Deg;
-            pivotObjectName.transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+            pivotObjectName.transform.rotation = Quaternion.Euler(0f, 0f, mouseRotationZ);
+
+        }
+
+        public void RotateEnvironment(float xInput)
+        {
+            environmentPivotObject.transform.Rotate(Vector3.back, xInput * rotationSpeed);
+        }
+
+        public void SetVectorInput(in Vector2 vectorInput)
+        {
+            xInput = vectorInput.x;
+            yInput = vectorInput.y;
+        }
+
+        public void SetMouseDirection()
+        {
+            mouseDifference = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position;
+            mouseDifference.Normalize();
+            mouseRotationZ = Mathf.Atan2(mouseDifference.y, mouseDifference.x) * Mathf.Rad2Deg;
+        }
+
+        public void BoostPlayer()
+        {
+            if (!GameManager.gameIsPaused)
+            {
+                if (boostCooldownLeftPercent == 1)
+                {
+                    playerRb.AddForce(mouseDifference * impulseForce, ForceMode2D.Impulse);
+                    boostNextFireTime = Time.time + boostCooldown;
+                    boostLastFireTime = Time.time;
+                    print((boostNextFireTime - Time.time) + " Seconds Left on the boost cooldown");
+
+
+                }
+                else
+                {
+                    print((boostNextFireTime - Time.time) + " Seconds Left on the boost cooldown");
+                }
+            }
+
 
         }
     }
